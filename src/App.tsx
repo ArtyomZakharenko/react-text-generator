99339a92e4d32d5873d60c50c002ce219da02e0b
Artyom Zakharenko <artiomzaharenko@gmail.com>
2022-09-14 13:37:29 +0300

    completed project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FormEvent, useState } from 'react';
+import data from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(1);
+	const [text, setText] = useState([] as string[]);
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		let amount = count.toString();
+		setText(data.slice(0, +amount));
+	};
+
+	return (
+		<section className='section-center'>
+			<h3>tired of boring lorem ipsum?</h3>
+			<form className='lorem-form' onSubmit={handleSubmit}>
+				<label htmlFor='amount'>paragraphs:</label>
+				<input
+					min='1'
+					max='8'
+					type='number'
+					name='amount'
+					id='amount'
+					value={count}
+					onChange={(e) => setCount(+e.target.value)}
+				/>
+				<button className='btn'>generate</button>
+			</form>
+			<article className='lorem-text'>
+				{text.map((item, index) => {
+					return <p key={index}>{item}</p>;
+				})}
+			</article>
+		</section>
+	);
 }
 
 export default App;
